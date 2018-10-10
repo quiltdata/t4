257aa2fdf6c9f60532f7f46c8082cc110f1981d1
nl_0 <nl.imbecile@gmail.com>
2018-10-10 23:45:31 +0500

    utils/AWS/Signer: s3 url signing

--- catalog/app/utils/AWS/Signer.js
+++ catalog/app/utils/AWS/Signer.js
@@ -1,6 +1,7 @@
 import SignerV4 from 'aws-sdk/lib/signers/v4';
+import PT from 'prop-types';
 import * as React from 'react';
-import { withPropsOnChange } from 'recompose';
+import { defaultProps, withPropsOnChange, setPropTypes } from 'recompose';
 
 import {
   composeComponent,
@@ -10,24 +11,48 @@ import {
 } from 'utils/reactTools';
 
 import * as Config from './Config';
+import * as S3 from './S3';
 
+
+const DEFAULT_URL_EXPIRATION = 5 * 60; // in seconds
 
 const Ctx = React.createContext();
 
+const parseS3Url = (url) => {
+  const withoutProto = url.replace(/^(https?:)?\/\//, '');
+  const match = withoutProto.match(/s3\.amazonaws\.com\/([a-z0-9-.]+)\/(.+)/);
+  return !!match && { bucket: match[1], key: match[2] };
+};
+
 export const Provider = composeComponent('AWS.Signer.Provider',
+  setPropTypes({
+    urlExpiration: PT.number,
+  }),
+  defaultProps({
+    urlExpiration: DEFAULT_URL_EXPIRATION,
+  }),
   Config.inject(),
-  withPropsOnChange(['awsConfig'], ({ awsConfig: { credentials } }) => ({
+  S3.inject(),
+  withPropsOnChange(['awsConfig', 's3'], ({
+    awsConfig: { credentials },
+    s3,
+    urlExpiration,
+  }) => ({
     signer: {
       signRequest: (request, serviceName) => {
-        // console.log('sign req', serviceName, credentials, request);
         if (!credentials) throw new Error('sign: no credentials');
         const signer = new SignerV4(request, serviceName);
         signer.addAuthorization(credentials, new Date());
-        // console.log('signed req', request);
       },
-      signURL: (url) => {
-        console.log('sign url', url);
-        return url;
+      signURLForBucket: (url, s3Bucket) => {
+        const info = parseS3Url(url);
+        return info && info.bucket === s3Bucket
+          ? s3.getSignedUrl('getObject', {
+            Bucket: s3Bucket,
+            Key: info.key,
+            Expires: urlExpiration,
+          })
+          : url;
       },
     },
   })),
