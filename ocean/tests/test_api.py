3a3c6173ea4d53e57d36e9300132c468af0b1596
Calvin Cochran <meffij@users.noreply.github.com>
2018-10-16 13:45:06 -0700

    Test search (#17)
    Also add .pylintrc

--- ocean/tests/test_api.py
+++ ocean/tests/test_api.py
@@ -1,13 +1,11 @@
+from unittest.mock import Mock, patch
 
 import pytest
-import requests
 import responses
 from ruamel.yaml import YAML
 
 import helium as he
 from helium import util
-
-
 
 class TestAPI():
     @responses.activate
@@ -38,10 +36,38 @@ class TestAPI():
         with pytest.raises(util.HeliumException, match='Port must be a number'):
             he.config('https://fliff:fluff')
 
-    
     def test_put_to_directory_failure(self):
         # Adding pathes with trailing delimeters causes AWS to treat them like virtual directories
         # and can cause issues when downloading to host machine.
         test_object = "foo"
         with pytest.raises(ValueError):
             he.put(test_object, "test/")
+
+    def test_search_no_config(self):
+        with pytest.raises(util.HeliumException, match="No configured region."):
+            he.search('*')
+
+    @patch('helium.api._create_es')
+    def test_search(self, _create_es):
+        mock_es_client = Mock()
+        mock_es_client.search.return_value = {
+            'took': 3,
+            'timed_out': False,
+            '_shards': {'total': 5, 'successful': 5, 'skipped': 0, 'failed': 0},
+            'hits': {'total': 0, 'max_score': None, 'hits': []}
+        }
+
+        _create_es.return_value = mock_es_client
+        query = '*'
+        payload = {'query': {'query_string': {
+            'default_field': 'content',
+            'query': query,
+            'quote_analyzer': 'keyword',
+        }}}
+
+        result = he.search(query)
+        assert mock_es_client.search.called
+        mock_es_client.search.assert_called_with(index=he.api.es_index, body=payload)
+
+        assert isinstance(result, list)
+        assert result == []
