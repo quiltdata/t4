3739034f63ba4d7cf58392d84af12271180c5f17
nivlac <meffij@gmail.com>
2018-10-26 11:49:21 -0700

    registry includes .quilt/

--- api/python/t4/snapshots.py
+++ api/python/t4/snapshots.py
@@ -171,7 +171,7 @@ def get_package_registry_root(path=''):
     """ Returns the package registry root for a given path """
     if path.startswith('s3://'):
         bucket = path[5:].partition('/')[0]
-        return "s3://{}".format(bucket)
+        return "s3://{}/.quilt/".format(bucket)
     else:
         # TODO: return local registry root
         raise NotImplementedError
@@ -240,14 +240,14 @@ class Package(object):
 
         if pkg_hash is not None:
             # if hash is specified, name doesn't matter
-            pkg_path = registry + '.quilt/packages/{}'.format(pkg_hash)
+            pkg_path = registry + 'packages/{}'.format(pkg_hash)
             # TODO replace open with something that supports both local and s3
             with open(pkg_path) as pkg_file:
                 pkg = self.load(pkg_file)
             self = pkg._clone()
             return
 
-        pkg_path = registry + '.quilt/named_packages/{}/'.format(name)
+        pkg_path = registry + 'named_packages/{}/'.format(name)
         # TODO: list files at this directory
         # TODO: default to latest version of named package
         raise NotImplementedError
