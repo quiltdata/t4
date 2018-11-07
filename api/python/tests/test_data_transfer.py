9d6d8741674fc4f34009c69a067080e1e6a13487
Brian <eode@eptitude.net>
2018-11-06 23:29:42 -0500

    another Format test

--- api/python/tests/test_data_transfer.py
+++ api/python/tests/test_data_transfer.py
@@ -6,7 +6,7 @@ try: import pathlib2 as pathlib
 except ImportError: import pathlib
 
 ### Project imports
-from t4.data_transfer import TargetType, deserialize_obj
+from t4.data_transfer import TargetType, deserialize_obj, Formats, Format
 
 ### Code
 def test_buggy_parquet():
@@ -18,3 +18,18 @@ def test_buggy_parquet():
     with open(path / 'data' / 'buggy_parquet.parquet', 'rb') as bad_parq:
         # Make sure this doesn't crash.
         deserialize_obj(bad_parq.read(), TargetType.PYARROW)
+
+def test_formats():
+    testdir = pathlib.Path(__file__).parent
+    import numpy as np
+
+    arr = np.ndarray(3)
+
+    fmt = Formats.for_obj(arr)
+
+    assert 'npz' in fmt._handled_extensions
+    assert Formats.for_ext('npy') is fmt
+    assert len(Formats.for_obj('blah', single=False)) == 2   # json, unicode
+    bytes_obj = fmt.serialize(arr)
+    assert np.array_equal(fmt.deserialize(bytes_obj), arr)
+
