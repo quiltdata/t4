feca36d105978f69befa0ca99cf1a88fe995b8de
nivlac <meffij@gmail.com>
2018-11-14 16:57:28 -0800

    Everything but deserialize, get_meta, and set_meta

--- api/python/t4/bucket.py
+++ api/python/t4/bucket.py
@@ -1,13 +1,30 @@
+import pathlib
 
+from .data_transfer import (copy_file, delete_object, list_objects,
+                            put_bytes, serialize_obj)
+from .util import QuiltException, fix_url
 
 class Bucket(object):
     """
     Implements Bucket interface for T4.
     """
-    def __init__(bucket_uri):
-        self._uri = bucket_uri
+    def __init__(self, bucket_uri):
+        """
+        Creates a Bucket object.
 
-    def deserialize(key):
+        Args:
+            bucket_uri(str): URI of bucket to target. Must start with 's3://'
+
+        Returns:
+            a new Bucket
+        """
+        if not bucket_uri.startswith('s3://'):
+            raise QuiltException("Bucket URI must start with s3://")
+        self._uri = bucket_uri.strip('/') + '/'
+        self._bucket = self._uri[5:] # remove 's3://'
+        self._bucket = self._bucket.strip('/') # remove trailing '/'
+
+    def deserialize(self, key):
         """
         Deserializes object at key from bucket.
 
@@ -23,13 +40,13 @@ class Bucket(object):
         """
         pass
 
-    def __call__(key):
+    def __call__(self, key):
         """
         Shorthand for deserialize(key)
         """
         return self.deserialize(key)
 
-    def put(key, obj, meta=None):
+    def put(self, key, obj, meta=None):
         """
         Stores obj at key in bucket, optionally with user-provided metadata.
 
@@ -38,10 +55,16 @@ class Bucket(object):
             obj(serializable): serializable object to store at key
             meta(dict): optional user-provided metadata to store
         """
+        dest = self._uri + key
         meta = meta or {}
-        pass
+        data, target = serialize_obj(obj)
+        all_meta = dict(
+            target=target.value,
+            user_meta=meta
+        )
+        put_bytes(data, fix_url(dest), all_meta)
 
-    def put_file(key, path):
+    def put_file(self, key, path):
         """
         Stores file at path to key in bucket.
 
@@ -56,9 +79,10 @@ class Bucket(object):
             if no file exists at path
             if copy fails
         """
-        pass
+        dest = self._uri + key
+        copy_file(path, dest)
 
-    def put_dir(key, directory):
+    def put_dir(self, key, directory):
         """
         Stores all files under directory under the prefix key.
 
@@ -73,18 +97,32 @@ class Bucket(object):
             if directory isn't a valid local directory
             if writing to bucket fails
         """
-        pass
+        # Ensure key ends in '/'.
+        if key[-1] != '/':
+            key = key + '/'
 
-    def keys():
+        src_path = pathlib.Path(directory)
+        if not src_path.is_dir():
+            raise QuiltException("Provided directory does not exist")
+
+        files = src_path.rglob('*')
+        for f in files:
+            if not f.is_file():
+                continue
+            new_key = key + f.relative_to(src_path).as_posix()
+            new_path = self._uri + new_key
+            copy_file(f.resolve().as_uri(), new_path)
+
+    def keys(self):
         """
         Lists all keys in the bucket.
 
         Returns:
             list of strings
         """
-        pass
+        return list(map(lambda x: x.get('Key'), list_objects(self._bucket, '')))
 
-    def delete(key):
+    def delete(self, key):
         """
         Deletes a key from the bucket.
 
@@ -97,9 +135,9 @@ class Bucket(object):
         Raises:
             if delete fails
         """
-        pass
+        delete_object(self._bucket, key)
 
-    def fetch(key, path):
+    def fetch(self, key, path):
         """
         Fetches file (or files) at key to path.
 
@@ -111,13 +149,28 @@ class Bucket(object):
             key(str): key in bucket to fetch
             path(str): path in local filesystem to store file or files fetched
 
+        Returns:
+            None
+
         Raises:
             if path doesn't exist
             if download fails
         """
-        pass
+        if not key.endswith('/'):
+            copy_file(self._uri + key, fix_url(path))
+            return
 
-    def get_meta(key):
+        objects = list_objects(self._bucket, key)
+        for o in objects:
+            okey = o['Key']
+            def remove_prefix(text, prefix):
+                return text[text.startswith(prefix) and len(prefix):]
+
+            relative_key = remove_prefix(okey, key)
+            new_location = path + relative_key
+            copy_object(fix_url(self._uri + okey), fix_url(new_location))
+
+    def get_meta(self, key):
         """
         Gets the metadata associated with a key in bucket.
 
@@ -132,7 +185,7 @@ class Bucket(object):
         """
         pass
 
-    def set_meta(key, meta):
+    def set_meta(self, key, meta):
         """
         Sets user metadata on key in bucket.
 
