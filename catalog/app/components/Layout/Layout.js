fa2372abf27c541ede0adaf4cba01998083a45e3
nl_0 <nl.imbecile@gmail.com>
2018-11-22 15:10:33 +0500

    adjust layout, refactor footer

--- catalog/app/components/Layout/Layout.js
+++ catalog/app/components/Layout/Layout.js
@@ -1,4 +1,7 @@
+import PT from 'prop-types';
 import * as React from 'react';
+import * as RC from 'recompose';
+import { withStyles } from '@material-ui/core/styles';
 
 import Footer from 'components/Footer';
 import { Pad } from 'components/LayoutHelpers';
@@ -7,13 +10,28 @@ import { composeComponent } from 'utils/reactTools';
 
 
 export default composeComponent('Layout',
-  ({ children, pre }) => (
-    <React.Fragment>
+  RC.setPropTypes({
+    children: PT.node,
+    pre: PT.node,
+  }),
+  withStyles(() => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+    },
+    spacer: {
+      flexGrow: 1,
+    },
+  })),
+  ({ classes, children, pre }) => (
+    <div className={classes.root}>
       <NavBar />
       {!!pre && pre}
       <Pad top left right bottom>
         {children}
       </Pad>
+      <div className={classes.spacer} />
       <Footer />
-    </React.Fragment>
+    </div>
   ));
