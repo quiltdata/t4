055d2ed177dc1fb1c937b2cbf3097f95b667ee64
nl_0 <nl.imbecile@gmail.com>
2018-11-23 15:40:46 +0500

    utils/NamedRoutes: remove wrapping proxy to avoid random exceptions

--- catalog/app/utils/NamedRoutes.js
+++ catalog/app/utils/NamedRoutes.js
@@ -13,21 +13,6 @@ import {
 
 const Ctx = React.createContext();
 
-// these props are being probed by react-devtools, so we shouldn't throw on them
-const IGNORE = [
-  '_reactFragment',
-  Symbol.iterator,
-  Symbol.toStringTag,
-];
-
-const ensureExists = (routes) => new Proxy(routes, {
-  get: (target, name) => {
-    if (name in target) return target[name];
-    if (IGNORE.includes(name)) return undefined;
-    throw new Error(`Route '${name}' does not exist`);
-  },
-});
-
 export const mkSearch = R.pipe(
   stringify,
   R.unless(R.isEmpty, (qs) => `?${qs}`),
@@ -35,8 +20,8 @@ export const mkSearch = R.pipe(
 
 export const Provider = composeComponent('NamedRoutes.Provider',
   withPropsOnChange(['routes'], ({ routes }) => ({
-    paths: ensureExists(R.pluck('path', routes)),
-    urls: ensureExists(R.pluck('url', routes)),
+    paths: R.pluck('path', routes),
+    urls: R.pluck('url', routes),
   })),
   provide(Ctx, R.pick(['paths', 'urls'])));
 
