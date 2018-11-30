0179ef591885a72a2b028e0d7d16512fbd281d59
nl_0 <nl.imbecile@gmail.com>
2018-11-30 14:20:39 +0500

    use error boundary for 404 handling

--- catalog/app/containers/App/App.js
+++ catalog/app/containers/App/App.js
@@ -1,6 +1,6 @@
 import * as R from 'ramda';
 import * as React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
 import {
   SignIn,
@@ -9,7 +9,7 @@ import {
 } from 'containers/AWSAuth';
 import Bucket from 'containers/Bucket';
 import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import { CatchNotFound, ThrowNotFound } from 'containers/NotFoundPage';
 import * as NamedRoutes from 'utils/NamedRoutes';
 import { injectSaga } from 'utils/SagaInjector';
 import { composeComponent } from 'utils/reactTools';
@@ -23,7 +23,7 @@ import saga from './saga';
 const requireAuthIfConfigured = config.alwaysRequiresAuth ? requireAuth : R.identity;
 
 const ProtectedHome = requireAuthIfConfigured(HomePage);
-const ProtectedNotFound = requireAuthIfConfigured(NotFoundPage);
+const ProtectedNotFound = requireAuthIfConfigured(ThrowNotFound);
 
 // eslint-disable-next-line react/prop-types
 const redirectTo = (path) => ({ location: { search } }) =>
@@ -32,13 +32,16 @@ const redirectTo = (path) => ({ location: { search } }) =>
 export default composeComponent('App',
   injectSaga(REDUX_KEY, saga),
   NamedRoutes.inject(),
-  ({ paths, urls }) => (
-    <Switch>
-      <Route path={paths.home} component={ProtectedHome} exact />
-      <Route path={paths.signIn} component={SignIn} exact />
-      <Route path="/login" component={redirectTo(urls.signIn())} exact />
-      <Route path={paths.signOut} component={SignOut} exact />
-      <Route path={paths.bucketRoot} component={requireAuth(Bucket)} />
-      <Route component={ProtectedNotFound} />
-    </Switch>
+  withRouter,
+  ({ location: l, paths, urls }) => (
+    <CatchNotFound key={`${l.pathname}${l.search}${l.hash}`}>
+      <Switch>
+        <Route path={paths.home} component={ProtectedHome} exact />
+        <Route path={paths.signIn} component={SignIn} exact />
+        <Route path="/login" component={redirectTo(urls.signIn())} exact />
+        <Route path={paths.signOut} component={SignOut} exact />
+        <Route path={paths.bucketRoot} component={requireAuth(Bucket)} />
+        <Route component={ProtectedNotFound} />
+      </Switch>
+    </CatchNotFound>
   ));
