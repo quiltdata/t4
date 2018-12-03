913f6d3a1c7eb1419625d73159fab7cdab263874
nl_0 <nl.imbecile@gmail.com>
2018-12-03 20:12:45 +0500

    bucket: getting started screen

--- catalog/app/containers/Bucket/Overview.js
+++ catalog/app/containers/Bucket/Overview.js
@@ -1,25 +1,32 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
+import * as NamedRoutes from 'utils/NamedRoutes';
 import * as RT from 'utils/reactTools';
 
 import Message from './Message';
 import Summary from './Summary';
 
 
+const DOC_LINK =
+  'https://github.com/quiltdata/t4/blob/master/UserDocs.md#using-the-catalog';
+const EXAMPLE_BUCKET = 'quilt-example';
+
 export default RT.composeComponent('Bucket.Overview',
-  ({ match: { params: { bucket } } }) => (
+  NamedRoutes.inject(),
+  ({ urls, match: { params: { bucket } } }) => (
     <Summary
       bucket={bucket}
       path=""
       progress
       whenEmpty={() => (
-        // TODO: get started guide
-        <Message headline="No overview">
-          <a
-            href="https://github.com/quiltdata/t4/blob/master/UserDocs.md#using-the-catalog"
-          >
-            Learn how to create an overview
-          </a>.
+        <Message headline="Getting Started">
+          Currently there`s no overview in this bucket
+          &mdash; <a href={DOC_LINK}>learn how to create one</a>.
+          <br />
+          Also, check out
+          our <Link to={urls.bucketRoot(EXAMPLE_BUCKET)}>example bucket</Link> to
+          see what T4 is capable of.
         </Message>
       )}
     />
