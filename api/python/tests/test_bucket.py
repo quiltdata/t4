8d7e69c022fce5a41b2258c9ff443b0776672da3
Brian Ealdwine <eode@eptitude.net>
2018-12-03 19:21:39 -0500

    Add test for Bucket.select()

--- api/python/tests/test_bucket.py
+++ api/python/tests/test_bucket.py
@@ -1,8 +1,13 @@
 import json
 
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
 import botocore.session
 from botocore.stub import Stubber
-
+import pandas as pd
 import pytest
 
 from t4 import Bucket
@@ -75,3 +80,85 @@ def test_bucket_fetch():
         stubber.add_response('list_objects_v2', response, params)
         with pytest.raises(QuiltException):
             Bucket('s3://test-bucket').fetch('does/not/exist/', './')
+
+
+def test_bucket_select():
+    # Stubber doesn't have an accurate shape for the results of select_object_content
+    chunks = [
+        b'{"foo": ',
+        b'9, "b',
+        b'ar": 3',
+        b'}\n{"foo"',
+        b': 9, "bar": 1}\n{"foo": 6, "bar": 9}\n{"foo":',
+        b' 1, "bar": 7}\n{"foo":',
+        b' 6, "bar": 1}\n{"foo": 6, "bar": 6}',
+        b'\n{"foo": 9, "bar": 6}',
+        b'\n{"foo": 6, "bar": 4}\n',
+        b'{"foo": 2, "bar": 0}',
+        b'\n{"foo": 2, "bar": 0}\n',
+        ]
+    records = [{'Records': {'Payload': chunk}} for chunk in chunks]
+    # noinspection PyTypeChecker
+    records.append({'Stats': {
+        'BytesScanned': 100,
+        'BytesProcessed': 100,
+        'BytesReturned': 210,
+        }})
+    records.append({'End': {}})
+
+    expected_result = pd.DataFrame.from_records([
+        {'foo': 9, 'bar': 3},
+        {'foo': 9, 'bar': 1},
+        {'foo': 6, 'bar': 9},
+        {'foo': 1, 'bar': 7},
+        {'foo': 6, 'bar': 1},
+        {'foo': 6, 'bar': 6},
+        {'foo': 9, 'bar': 6},
+        {'foo': 6, 'bar': 4},
+        {'foo': 2, 'bar': 0},
+        {'foo': 2, 'bar': 0},
+        ])
+
+    # test normal use from extension
+    expected_args = {
+        'Bucket': 'test-bucket',
+        'Key': 'test',
+        'Expression': 'select * from S3Object',
+        'ExpressionType': 'SQL',
+        'InputSerialization': {
+            'CompressionType': 'NONE',
+            'JSON': {'Type': 'DOCUMENT'}
+            },
+        'OutputSerialization': {'JSON': {}},
+        }
+    boto_return_val = {'Payload': iter(records)}
+    patched_s3 = mock.patch.object(
+        s3_client,
+        'select_object_content',
+        return_value=boto_return_val,
+        autospec=True,
+    )
+
+    test_meta = {
+        'helium': json.dumps({'target': 'json'})
+    }
+    response = {
+        'Metadata': test_meta,
+        'ContentLength': 123
+    }
+    params = {
+        'Bucket': 'test-bucket',
+        'Key': 'test'
+    }
+
+    with Stubber(s3_client) as stubber:
+        stubber.add_response('head_object', response, params)
+        with patched_s3 as patched:
+            bucket = Bucket('s3://test-bucket')
+
+            result = bucket.select('test', 'select * from S3Object')
+
+            patched.assert_called_once_with(**expected_args)
+            assert result.equals(expected_result)
+
+    # Further testing specific to select() is in test_data_transfer
