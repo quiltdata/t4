9c09bfd58e065b1332a810e6db076dbebe03bece
nivlac <meffij@gmail.com>
2018-12-06 14:06:17 -0800

    Update configs

--- catalog/static/config.js
+++ catalog/static/config.js
@@ -10,17 +10,26 @@ window.__CONFIG = {
     elasticSearchUrl: 'https://search-t4-stack-vh6cadfyzemgwbdmpfjqenq36a.us-east-1.es.amazonaws.com'
   },
   configs: {
+    "alpha-quilt-storage": {
+      icon: 'https://d1zvn9rasera71.cloudfront.net/q-128-square.png',
+      title: 'Quilt Alpha',
+      description: '',
+      // eslint-disable-next-line comma-dangle
+      search_endpoint: 'https://search-alpha-ness-i55uw3a7cpm3aejv22y7p4ijge.us-east-1.es.amazonaws.com/'
+    },
     "quilt-example": {
-      icon: 'https://quiltcorp.files.wordpress.com/2017/08/q.png',
-      title: 'Quilt Data',
-      description: 'Turbocharging your S3 Bucket',
+      menu: true,
+      icon: 'https://d1zvn9rasera71.cloudfront.net/q-128-square.png',
+      title: 'Quilt Example',
+      description: 'T4 transforms an S3 bucket into a visual data regsitry',
       // eslint-disable-next-line comma-dangle
       search_endpoint: 'https://search-quilt-example-rytakqayno3rlembetdhuflrna.us-east-1.es.amazonaws.com/'
     },
     "quilt-aics": {
+      menu: true,
       icon: 'https://3c1703fe8d.site.internapcdn.net/newman/gfx/news/hires/2017/3dwindowinto.png',
       title: 'Allen Institute for Cell Science',
-      description: 'Launched with a contribution from Paul G. Allen in 2014, the Allen Institute for Cell Science will serve as a catalyzing force to integrate diverse technologies and approaches at a large scale in order to study the cell as an integrated system: something that traditional academic labs cannot do. Our inaugural project, the Allen Cell Explorer, will be a novel visual, dynamic, predictive model of the cell that will accelerate cell biology and biomedical research.',
+      description: 'The Allen Cell Explorer is a novel visual, dynamic, predictive model of the cell that will accelerate cell biology and biomedical research.',
       // eslint-disable-next-line comma-dangle
       search_endpoint: 'https://search-quilt-aics-2alivodd7we6cjhcrkxguahbkm.us-east-1.es.amazonaws.com/'
     // eslint-disable-next-line comma-dangle
