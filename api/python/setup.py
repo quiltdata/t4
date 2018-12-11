41b3449912c908073abebc10b391fb2391f1fa1e
stevededalus <michael.sindelar@gmail.com>
2018-12-11 09:39:15 -0800

    WIP CICD for PyPi

--- api/python/setup.py
+++ api/python/setup.py
@@ -1,9 +1,28 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+VERSION = "0.0.1"
 
 def readme():
     readme_short = """
     """
     return readme_short
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
 
 setup(
     name="t4",
@@ -59,5 +78,8 @@ setup(
     include_package_data=True,
     entry_points={
         # 'console_scripts': ['quilt=quilt.tools.main:main'],
-    }
+    },
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }    
 )
