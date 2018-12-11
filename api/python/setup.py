c7a7f64ca60d52fa085a9ee4fb9a49cbcfec0f62
stevededalus <michael.sindelar@gmail.com>
2018-12-11 15:21:01 -0800

    Bump to 0.0.4 (#171)

--- api/python/setup.py
+++ api/python/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 def readme():
     readme_short = """
