12f7d17b698770dc8b8ed0725a1e6b63143d8f6c
Kevin Moore <kevin@quiltdata.io>
2018-12-21 15:32:40 -0800

    Parse s3 path from URL
    Isolate the s3 path (key) from the URL when checking for file extension since the URL might have query parameters etc. after the s3 key.

--- lambdas/nbconvert/index.py
+++ lambdas/nbconvert/index.py
@@ -3,6 +3,7 @@ import urllib.request
 from nbconvert import HTMLExporter
 import nbformat
 import pandas as pd
+from urlib.parse import urlparse
 
 html_exporter = HTMLExporter()
 html_exporter.template_file = 'full'
@@ -15,8 +16,8 @@ def lambda_handler(event, context):
             "statusCode": 400
         }
 
-    # TODO: parse the URL and check the actual key
-    if url.endswith('.parquet'):
+    parse = urlparse(url)
+    if parse.path.endswith('.parquet'):
         df = pd.read_parquet(url)
         body = df._repr_html_()
     else:
