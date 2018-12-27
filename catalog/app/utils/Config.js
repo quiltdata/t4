e2e0a6de7560f723029718c2ea43ddc7c528ba3a
nl_0 <nl.imbecile@gmail.com>
2018-12-27 14:19:02 +0500

    config: add guestCredentials

--- catalog/app/utils/Config.js
+++ catalog/app/utils/Config.js
@@ -26,6 +26,10 @@ const validateConfig = conforms({
   defaultBucket: R.is(String),
   signInRedirect: R.is(String),
   signOutRedirect: R.is(String),
+  guestCredentials: conforms({
+    accessKeyId: R.is(String),
+    secretAccessKey: R.is(String),
+  }),
   suggestedBuckets: isArrayOf(R.is(String)),
   federations: isArrayOf(R.is(String)),
 });
