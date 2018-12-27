f336dcef9d37ec1681f8d6ee35f23d06d1feb3ae
nl_0 <nl.imbecile@gmail.com>
2018-12-27 19:52:32 +0500

    Bucket: proper message for guest access error

--- catalog/app/containers/Bucket/errors.js
+++ catalog/app/containers/Bucket/errors.js
@@ -1,7 +1,15 @@
 import * as R from 'ramda';
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Route, Link } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
+import * as Auth from 'containers/AWSAuth';
+import * as NamedRoutes from 'utils/NamedRoutes';
 import { BaseError } from 'utils/error';
+import * as RT from 'utils/reactTools';
 
 import Message from './Message';
 
@@ -12,7 +20,41 @@ export class AccessDenied extends BucketError {}
 
 export class CORSError extends BucketError {}
 
-export const displayError = (pairs = []) => R.cond([
+const WhenAuth = connect(createStructuredSelector({
+  authenticated: Auth.selectors.authenticated,
+}))(({ authenticated, cases, args }) => cases[authenticated](...args));
+
+const whenAuth = (cases) => (...args) => <WhenAuth {...{ cases, args }} />;
+
+const SignIn = RT.composeComponent('Bucket.errors.SignIn',
+  withStyles(({ palette }) => ({
+    root: {
+      '&, &:visited': {
+        color: palette.primary.contrastText,
+      },
+    },
+  })),
+  ({ classes }) => (
+    <NamedRoutes.Inject>
+      {({ urls }) => (
+        <Route>
+          {({ location: l }) => (
+            <Button
+              component={Link}
+              to={urls.signIn(l.pathname + l.search + l.hash)}
+              variant="contained"
+              color="primary"
+              className={classes.root}
+            >
+              Sign In
+            </Button>
+          )}
+        </Route>
+      )}
+    </NamedRoutes.Inject>
+  ));
+
+const defaultHandlers = [
   [R.is(CORSError), () => (
     <Message headline="Error">
       Seems like this bucket is not configured for T4.
@@ -24,15 +66,29 @@ export const displayError = (pairs = []) => R.cond([
       </a>.
     </Message>
   )],
-  [R.is(AccessDenied), () => (
-    <Message headline="Access Denied">
-      Seems like you don`t have access to this bucket.
-      <br />
-      <a href="https://github.com/quiltdata/t4/tree/master/deployment#permissions">
-        Learn about access control in T4
-      </a>.
-    </Message>
-  )],
+  [R.is(AccessDenied), whenAuth({
+    true: () => (
+      <Message headline="Access Denied">
+        Seems like you don`t have access to this bucket.
+        <br />
+        <a href="https://github.com/quiltdata/t4/tree/master/deployment#permissions">
+          Learn about access control in T4
+        </a>.
+      </Message>
+    ),
+    false: () => (
+      <Message headline="Access Denied">
+        Anonymous access not allowed. Please sign in.
+        <br />
+        <br />
+        <SignIn />
+      </Message>
+    ),
+  })],
+];
+
+export const displayError = (pairs = []) => R.cond([
+  ...defaultHandlers,
   ...pairs,
   [R.T, (e) => { throw e; }],
 ]);
