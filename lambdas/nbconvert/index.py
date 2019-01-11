550c28d598b65fe86ec1517287b8b61f9c8c179c
Aneesh Karve <aneesh.karve@gmail.com>
2019-01-10 18:21:11 -0800

    wip : add parquet metadata; use bytesio

--- lambdas/nbconvert/index.py
+++ lambdas/nbconvert/index.py
@@ -1,8 +1,17 @@
-import urllib.request
+"""
+Return preview for given file type.
+Currently handles:
+* .parquet
+* Ipynb
+"""
+import io
+import json
 
 from nbconvert import HTMLExporter
 import nbformat
 import pandas as pd
+import pyarrow as pa
+import urllib.request
 from urllib.parse import urlparse
 
 html_exporter = HTMLExporter()
@@ -17,12 +26,22 @@ def lambda_handler(event, context):
         }
 
     parse = urlparse(url)
-    if parse.path.endswith('.parquet'):
+    response = urllib.request.urlopen(url) 
+    filelike = io.BytesIO(response.read())
+
+    if parse.path.lower().endswith('.parquet'):
+        # TODO: make this memory efficient for large files (use slicing?)
         df = pd.read_parquet(url)
-        body = df._repr_html_()
-    else:
-        response = urllib.request.urlopen(url).read().decode()
-        notebook = nbformat.reads(response, 4)
+        filelike.seek(0) # reset so pyarrow can read again
+        meta = pa.read_metadata(filelike)
+        moremeta = {k.decode():json.loads(meta.metadata[k]) for k in meta.metadata}
+        body = json.dumps({
+            'metadata': moremeta, 
+            'summary': str(meta),
+            'table_html': df._repr_html_(),
+        })
+    elif parse.path.lower().endswith('.ipynb'):
+        notebook = nbformat.reads(filelike.read.decode(), 4)
         body, resources = html_exporter.from_notebook_node(notebook)
 
     return {
