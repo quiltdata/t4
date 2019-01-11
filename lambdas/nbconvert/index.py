572becad2985dda742b067802e15747fd69e65ed
Dima Ryazanov <dima@gmail.com>
2019-01-10 17:13:00 -0800

    Implement CORS headers and JSON response type
    - Return CORS headers if the origin is correct
    - Return JSON rather than plain HTML if output=json is specified
    - Add a parameter for nbconvert template file
    - Add a parameter to explicitly set input type

--- lambdas/nbconvert/index.py
+++ lambdas/nbconvert/index.py
@@ -1,34 +1,105 @@
+from enum import Enum
+import json
+import os
 import urllib.request
 
+from jsonschema import Draft4Validator, ValidationError
 from nbconvert import HTMLExporter
 import nbformat
 import pandas as pd
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
 
-html_exporter = HTMLExporter()
-html_exporter.template_file = 'full'
+WEB_ORIGIN = os.environ['WEB_ORIGIN']
+
+
+SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'url': {
+            'type': 'string'
+        },
+        'output': {
+            'enum': ['html', 'json']
+        },
+        'input': {
+            'enum': ['ipynb', 'parquet']
+        },
+        'nbconvert_template': {
+            'enum': ['full', 'basic']
+        }
+    },
+    'required': ['url'],
+    'additionalProperties': False
+}
+
+Draft4Validator.check_schema(SCHEMA)
+VALIDATOR = Draft4Validator(SCHEMA)
+
 
 def lambda_handler(event, context):
-    url = event['queryStringParameters'].get('url')
-    if not url:
+    params = event['queryStringParameters'] or {}
+    headers = event['headers'] or {}
+
+    try:
+        VALIDATOR.validate(params)
+    except ValidationError as ex:
         return {
-            "body": "Missing 'url'",
+            "body": ex.message,
             "statusCode": 400
         }
 
-    parse = urlparse(url)
-    if parse.path.endswith('.parquet'):
-        df = pd.read_parquet(url)
-        body = df._repr_html_()
-    else:
+    url = params['url']
+    input_type = params.get('input')
+
+    # TODO(dima): Make it required.
+    if input_type is None:
+        parsed_url = urlparse(url)
+        path = unquote(parsed_url.path)
+        if path.endswith('.parquet'):
+            input_type = 'parquet'
+        else:
+            input_type = 'ipynb'
+
+
+    if input_type == 'ipynb':
+        html_exporter = HTMLExporter()
+        html_exporter.template_file = params.get('nbconvert_template', 'full')
+
         response = urllib.request.urlopen(url).read().decode()
         notebook = nbformat.reads(response, 4)
-        body, resources = html_exporter.from_notebook_node(notebook)
+        result, _ = html_exporter.from_notebook_node(notebook)
+    elif input_type == 'parquet':
+        df = pd.read_parquet(url)
+        result = df._repr_html_()
+    else:
+        assert False
+
+    output = params.get('output', 'html')
+
+    if output == 'json':
+        body = json.dumps({
+            'body': result
+        })
+        content_type = 'application/json'
+    elif output == 'html':
+        body = result
+        content_type = 'text/html'
+    else:
+        assert False
+
+    response_headers = {
+        "Content-Type": content_type
+    }
+    if headers.get('origin') == WEB_ORIGIN:
+        response_headers.update({
+            'access-control-allow-origin': '*',
+            'access-control-allow-methods': 'GET',
+            'access-control-allow-headers': '*',
+            'access-control-max-age': 86400
+        })
 
     return {
         "statusCode": 200,
         "body": body,
-        "headers": {
-            "Content-Type": "text/html"
-        }
+        "headers": response_headers
     }
--- lambdas/nbconvert/setup.py
+++ lambdas/nbconvert/setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     py_modules=['index'],
     install_requires=[
+        'jsonschema==2.6.0',
         'nbconvert==5.3.1',
         'pandas==0.23.4',
         'pyarrow==0.11.1',
