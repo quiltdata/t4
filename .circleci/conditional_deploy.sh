13f7547d5be2f6d17d942719de111ff928d15a7a
stevededalus <michael.sindelar@gmail.com>
2019-01-25 09:10:59 -0800

    First pass at deploy on master

---
+++ .circleci/conditional_deploy.sh
@@ -0,0 +1,16 @@
+set -e
+
+# latest commit
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+# latest commit where deployment/ was changed
+DEPLOY_COMMIT=$(git log -1 --format=format:%H --full-diff deployment/)
+
+if [ $DEPLOY_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "Files in deployment/ have changed."
+        .deploy.sh
+else
+     echo "No folders of relevance has changed."
+     exit 0;
+fi
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+
+#! /bin/bash
+
+set -exu
+
+aws cloudformation update-stack \
+          --stack-name t4-staging \
+          --capabilities CAPABILITY_NAMED_IAM \
+          --use-previous-template \
+          --parameters \
+            ParameterKey=QuiltWebHost,ParameterValue=t4-stage.quiltdata.com \
+            ParameterKey=ConfigBucketName,ParameterValue=t4-staging-config	 \
+            ParameterKey=QuiltBucketName,ParameterValue=t4-staging \
+            ParameterKey=Users,UsePreviousValue=true \
+            ParameterKey=CertificateArn,UsePreviousValue=true \
