4031177f7ecfdca8803977060aa28031fb97d7b2
stevededalus <michael.sindelar@gmail.com>
2019-01-28 16:54:34 -0800

    Update

--- .circleci/conditional_deploy.sh
+++ .circleci/conditional_deploy.sh
@@ -10,7 +10,7 @@ DEPLOY_COMMIT=$(git log -1 --format=format:%H --full-diff deployment/)
 if [ $DEPLOY_COMMIT = $LATEST_COMMIT ];
     then
         echo "Files in deployment/ have changed."
-        deploy.sh
+        ./deploy.sh
 else
      echo "No folders of relevance has changed."
      exit 0;
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -exu
 aws cloudformation update-stack \
           --stack-name t4-staging \
           --capabilities CAPABILITY_NAMED_IAM \
-          --use-previous-template \
+          --template-body file://deployment/t4.yaml \
           --parameters \
             ParameterKey=QuiltWebHost,ParameterValue=t4-stage.quiltdata.com \
             ParameterKey=ConfigBucketName,ParameterValue=t4-staging-config	 \
