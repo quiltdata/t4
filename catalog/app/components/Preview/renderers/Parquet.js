cc7be8a07aa5dbadfc542f3a45e073050ca412ea
nl_0 <nl.imbecile@gmail.com>
2019-02-12 19:44:05 +0500

    Preview: tweak parquet preview styling

--- catalog/app/components/Preview/renderers/Parquet.js
+++ catalog/app/components/Preview/renderers/Parquet.js
@@ -1,7 +1,32 @@
+import cx from 'classnames';
+import PT from 'prop-types';
 import * as React from 'react';
+import * as RC from 'recompose';
+import { withStyles } from '@material-ui/core/styles';
+
+import * as RT from 'utils/reactTools';
 
 
-// TODO
+const Parquet = RT.composeComponent('Preview.renderers.Parquet',
+  RC.setPropTypes({
+    children: PT.string,
+    className: PT.string,
+  }),
+  withStyles(({ spacing: { unit } }) => ({
+    root: {
+      overflow: 'auto',
+      padding: unit,
+      width: '100%',
+    },
+  })),
+  ({ classes, children, className, ...props } = {}) => (
+    <div
+      className={cx(className, classes.root)}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: children }}
+      {...props}
+    />
+  ));
+
 export default ({ preview }, props) =>
-  // eslint-disable-next-line react/no-danger
-  <div dangerouslySetInnerHTML={{ __html: preview }} {...props} />;
+  <Parquet {...props}>{preview}</Parquet>;
