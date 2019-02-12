96d74a18f30ffcb30e4e8117d08f79f684a81fbf
nl_0 <nl.imbecile@gmail.com>
2019-02-12 18:29:47 +0500

    Preview: render math in notebooks

--- catalog/app/components/Preview/renderers/Notebook.js
+++ catalog/app/components/Preview/renderers/Notebook.js
@@ -1,12 +1,33 @@
 import cx from 'classnames';
+import PT from 'prop-types';
 import * as React from 'react';
+import * as RC from 'recompose';
+import { withStyles } from '@material-ui/core/styles';
+
+import 'katex/dist/katex.css';
+
+import * as RT from 'utils/reactTools';
 
 
-export default ({ preview }, { className, ...props } = {}) => (
-  <div
-    className={cx(className, 'ipynb-preview')}
-    // eslint-disable-next-line react/no-danger
-    dangerouslySetInnerHTML={{ __html: preview }}
-    {...props}
-  />
-);
+const Notebook = RT.composeComponent('Preview.renderers.Notebook',
+  RC.setPropTypes({
+    children: PT.string,
+    className: PT.string,
+  }),
+  withStyles(({ spacing: { unit } }) => ({
+    root: {
+      padding: unit,
+      width: '100%',
+    },
+  })),
+  ({ classes, children, className, ...props } = {}) => (
+    <div
+      className={cx(className, classes.root, 'ipynb-preview')}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: children }}
+      {...props}
+    />
+  ));
+
+export default ({ preview }, props) =>
+  <Notebook {...props}>{preview}</Notebook>;
