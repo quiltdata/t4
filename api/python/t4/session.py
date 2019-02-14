a473f3f42272bebc9358faa28a1433cd72106875
nivlac <meffij@gmail.com>
2019-02-13 16:02:13 -0800

    Add login_user_pass for login without catalog

--- api/python/t4/session.py
+++ api/python/t4/session.py
@@ -176,6 +176,24 @@ def login():
 
     login_with_token(refresh_token)
 
+def login_user_pass(username, password):
+    """
+    Get a token with username and password.
+    """
+    data = {
+        'username': username,
+        'password': password
+    }
+    response = requests.post(
+        "{url}/api/login".format(
+            url=get_registry_url()
+        ),
+        json=data
+    )
+    assert response.ok
+    token = response.json()['token']
+    login_with_token(token)
+
 def login_with_token(refresh_token):
     """
     Authenticate using an existing token.
