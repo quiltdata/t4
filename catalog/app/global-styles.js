0c315a9e93fc3173d8c2f57253ef3f474fdbb27f
Aneesh Karve <aneesh.karve@gmail.com>
2019-02-15 15:00:07 -0800

    make parquet table styling look a bit more like pandas

--- catalog/app/global-styles.js
+++ catalog/app/global-styles.js
@@ -116,6 +116,19 @@ injectGlobal`
     table, th, td {
       border: 1px solid #ddd;
     }
+
+    table.dataframe, .dataframe th, .dataframe td  {
+      border: none;
+      font-size: small;
+
+      tr:nth-child(even) {
+        background-color: #f5f5f5;
+      }
+    }
+
+    .dataframe td {
+      white-space: nowrap;
+    }
   }
 
   /* per app.js fontObservers, fontLoaded is fired once Roboto is ready */
