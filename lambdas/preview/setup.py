ce47377926d3c9c9b678de3f4506f1b4716cbc8e
Dima Ryazanov <dima@gmail.com>
2019-02-21 11:22:39 -0800

    Clean up test requirements

--- lambdas/preview/setup.py
+++ lambdas/preview/setup.py
@@ -4,12 +4,4 @@ setup(
     name='t4_lambda_preview',
     version='0.0.1',
     py_modules=['index'],
-    extras_require={
-        'tests': [
-            'codecov',
-            'pytest',
-            'responses',
-            'pytest-cov',
-        ],
-    },
 )
