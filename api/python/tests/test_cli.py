69d6e55b482bb1d59dcf8bd82df509b11dad961c
nivlac <meffij@gmail.com>
2019-02-25 10:25:21 -0800

    Add CLI autoconfig

---
+++ api/python/tests/test_cli.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from t4 import main
+
+create_parser = main.create_parser
+
+class CommandLineTestCase(TestCase):
+    """
+    Base TestCase class, sets up a CLI parser
+    """
+    @classmethod
+    def setUpClass(cls):
+        parser = create_parser()
+        cls.parser = parser
+
+class PingTestCase(CommandLineTestCase):
+    def test_t4_config(self):
+        args = self.parser.parse_args(['config', 'https://foo.bar'])
+        assert args.catalog_url == 'https://foo.bar'
+        assert args.func == main.api.config
