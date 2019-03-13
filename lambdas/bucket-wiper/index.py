63cb2e5aa86d77187389b13c61ea6daa60812483
stevededalus <michael.sindelar@gmail.com>
2019-03-12 17:39:30 -0700

    Delete versioned objects

--- lambdas/bucket-wiper/index.py
+++ lambdas/bucket-wiper/index.py
@@ -13,9 +13,12 @@ def lambda_handler(event, context):
         if event['RequestType'] == 'Delete':
             s3 = boto3.resource('s3')
             bucket = s3.Bucket(bucket)
-            for obj in bucket.objects.filter():
-                s3.Object(bucket.name, obj.key).delete()
-
+            # for obj_version in bucket.object_versions.all():
+            versioned_objs = []
+            for obj_version in bucket.object_versions.all():
+                versioned_objs.append({'Key': obj_version.object_key,
+                                       'VersionId': obj_version.id})
+            bucket.delete_objects(Delete={'Objects': versioned_objs})
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
     except Exception as e:
         print(e)
