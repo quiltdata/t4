3e635b9bde8d247dea33e770297aabbd483a8c74
nl_0 <nl.imbecile@gmail.com>
2019-03-15 00:33:31 +0500

    components/Layout: add dark bg variant, use new MUI

--- catalog/app/components/Layout/Layout.js
+++ catalog/app/components/Layout/Layout.js
@@ -1,10 +1,9 @@
 import PT from 'prop-types';
 import * as React from 'react';
 import * as RC from 'recompose';
-import { withStyles } from '@material-ui/styles';
+import { unstable_Box as Box } from '@material-ui/core/Box';
 
 import Footer from 'components/Footer';
-import { Pad } from 'components/LayoutHelpers';
 import * as NavBar from 'containers/NavBar';
 import { composeComponent } from 'utils/reactTools';
 
@@ -14,26 +13,19 @@ export default composeComponent('Layout',
     children: PT.node,
     pre: PT.node,
     bare: PT.bool,
+    dark: PT.bool,
   }),
-  withStyles(({ palette }) => ({
-    root: {
-      background: palette.background.default,
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-    },
-    spacer: {
-      flexGrow: 1,
-    },
-  })),
-  ({ bare, classes, children, pre }) => (
-    <div className={classes.root}>
+  ({ bare = false, dark = false, children, pre }) => (
+    <Box
+      bgcolor={dark ? 'primary.main' : 'background.default'}
+      display="flex"
+      flexDirection="column"
+      minHeight="100vh"
+    >
       {bare ? <NavBar.Container /> : <NavBar.NavBar />}
       {!!pre && pre}
-      <Pad top left right bottom>
-        {children}
-      </Pad>
-      <div className={classes.spacer} />
+      <Box p={4}>{children}</Box>
+      <Box flexGrow={1} />
       <Footer />
-    </div>
+    </Box>
   ));
