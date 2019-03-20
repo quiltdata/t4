368789583fa67824af3dbabbeb5a855277c21a57
nl 0 <nl.imbecile@gmail.com>
2019-03-21 04:40:39 +0500

    Catalog: Add Intercom (#335)

--- catalog/app/containers/HomePage/HomePage.js
+++ catalog/app/containers/HomePage/HomePage.js
@@ -4,45 +4,43 @@ import Button from '@material-ui/core/Button';
 
 import FAIcon from 'components/FAIcon';
 import Feature from 'components/Feature';
+import * as Intercom from 'components/Intercom';
 import Layout from 'components/Layout';
 import * as urls from 'constants/urls';
 
 
-const openIntercom = () => {
-  if (window.Intercom) {
-    window.Intercom('show');
-  }
+export default () => {
+  const intercom = Intercom.use();
+  return (
+    <Layout
+      dark
+      pre={
+        <React.Fragment>
+          <Feature
+            header="Collaborate in S3"
+            tagline="Search, visualize, and version with the Quilt Data Catalog"
+          />
+          <Box
+            color="common.white"
+            display="flex"
+            p={2}
+          >
+            <Box flexGrow={1} />
+            <Button href={urls.slackInvite} color="inherit">
+              <FAIcon type="slack" />&nbsp;Join Slack
+            </Button>
+            <Box width="1em" />
+            <Button href={urls.t4Docs} color="inherit">
+              <FAIcon type="book" />&nbsp;Read Docs
+            </Button>
+            <Box width="1em" />
+            <Button onClick={() => intercom('show')} color="inherit">
+              <FAIcon type="chatBubble" />&nbsp;Chat
+            </Button>
+          </Box>
+        </React.Fragment>
+      }
+    >
+    </Layout>
+  );
 };
-
-export default () => (
-  <Layout
-    dark
-    pre={
-      <React.Fragment>
-        <Feature
-          header="Collaborate in S3"
-          tagline="Search, visualize, and version with the Quilt Data Catalog"
-        />
-        <Box
-          color="common.white"
-          display="flex"
-          p={2}
-        >
-          <Box flexGrow={1} />
-          <Button href={urls.slackInvite} color="inherit">
-            <FAIcon type="slack" />&nbsp;Join Slack
-          </Button>
-          <Box width="1em" />
-          <Button href={urls.t4Docs} color="inherit">
-            <FAIcon type="book" />&nbsp;Read Docs
-          </Button>
-          <Box width="1em" />
-          <Button onClick={openIntercom} color="inherit">
-            <FAIcon type="chatBubble" />&nbsp;Chat
-          </Button>
-        </Box>
-      </React.Fragment>
-    }
-  >
-  </Layout>
-);
