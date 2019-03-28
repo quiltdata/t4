6ff744623a2fff1d40934a382f8af55300fa6235
ResidentMario <aleksey.bilogur@gmail.com>
2019-03-27 17:51:39 -0700

    Fix the other error.

--- catalog/app/containers/Bucket/errors.js
+++ catalog/app/containers/Bucket/errors.js
@@ -60,7 +60,7 @@ const defaultHandlers = [
       <Message headline="Access Denied">
         Seems like you don&apos;t have access to this bucket.
         <br />
-        <StyledLink href="https://github.com/quiltdata/t4/tree/master/deployment#permissions">
+        <StyledLink href="https://quiltdocs.gitbook.io/t4/walkthrough/working-with-the-catalog#brief-tour>
           Learn about access control in T4
         </StyledLink>.
       </Message>
