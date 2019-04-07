c5387bb0a67a514dd91f051faeaf230b6b9ff06a
eode <aeon.descriptor@gmail.com>
2019-04-07 13:58:09 -0400

    Update and cleanup re: search (#359)
    * Remove api.search and usages
    * Remove api.log() (no usages)
    * Fix search for buckets
    * Pass bucket region to ES for signing
    * improve Bucket.config() exception msg when navigator_url not set
    * Added test for Bucket().search() including region.
    * minor config cleanup, more needed in separate PR

--- api/python/tests/test_api.py
+++ api/python/tests/test_api.py
@@ -54,35 +54,6 @@ class TestAPI(QuiltTestCase):
         with pytest.raises(ValueError):
             he.put(test_object, "s3://test/")
 
-    def test_search_no_config(self):
-        with pytest.raises(util.QuiltException, match="No configured region."):
-            he.search('*')
-
-    @patch('t4.api._create_es')
-    def test_search(self, _create_es):
-        mock_es_client = Mock()
-        mock_es_client.search.return_value = {
-            'took': 3,
-            'timed_out': False,
-            '_shards': {'total': 5, 'successful': 5, 'skipped': 0, 'failed': 0},
-            'hits': {'total': 0, 'max_score': None, 'hits': []}
-        }
-
-        _create_es.return_value = mock_es_client
-        query = '*'
-        payload = {'query': {'query_string': {
-            'default_field': 'content',
-            'query': query,
-            'quote_analyzer': 'keyword',
-        }}}
-
-        result = he.search(query)
-        assert mock_es_client.search.called
-        mock_es_client.search.assert_called_with(index=he.api.es_index, body=payload)
-
-        assert isinstance(result, list)
-        assert result == []
-
     def test_put_copy_get(self):
         data = np.array([1, 2, 3])
         meta = {'foo': 'bar', 'x': 42}
