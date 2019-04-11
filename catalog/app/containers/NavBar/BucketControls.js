201f6115bbba0fe5558426ee19670150f671e4fd
nl_0 <nl.imbecile@gmail.com>
2019-04-11 16:53:51 +0500

    NavBar/BucketControls: refactor

--- catalog/app/containers/NavBar/BucketControls.js
+++ catalog/app/containers/NavBar/BucketControls.js
@@ -1,6 +1,7 @@
 import PT from 'prop-types';
 import * as React from 'react';
 import * as RC from 'recompose';
+import { unstable_Box as Box } from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import { withStyles } from '@material-ui/styles';
@@ -43,46 +44,34 @@ const BucketDisplay = RT.composeComponent('NavBar.BucketControls.BucketDisplay',
     </Button>
   ));
 
-const dispatchOn = (prop, cases) => ({ [prop]: value, ...props }) =>
-  cases[value](props);
+// eslint-disable-next-line react/prop-types
+const BucketDisplaySelect = ({ bucket }) => {
+  const [selecting, setSelecting] = React.useState(false);
+  const select = React.useCallback(() => {
+    setSelecting(true);
+  }, []);
+  const cancel = React.useCallback(() => {
+    setSelecting(false);
+  }, []);
 
-const BucketDisplaySelect = RT.composeComponent('NavBar.BucketControls.BucketDisplaySelect',
-  RC.setPropTypes({
-    bucket: PT.string.isRequired,
-  }),
-  RC.withStateHandlers({
-    selecting: false,
-  }, {
-    select: () => () => ({ selecting: true }),
-    cancel: () => () => ({ selecting: false }),
-  }),
-  dispatchOn('selecting', {
-    // eslint-disable-next-line react/prop-types
-    true: ({ cancel }) => <BucketSelect autoFocus cancel={cancel} />,
-    // eslint-disable-next-line react/prop-types
-    false: ({ bucket, select }) =>
-      <BucketDisplay bucket={bucket} select={select} />,
-  }));
+  return selecting
+    ? <BucketSelect autoFocus cancel={cancel} />
+    : <BucketDisplay bucket={bucket} select={select} />;
+};
 
-export default RT.composeComponent('NavBar.BucketControls',
-  withStyles(() => ({
-    root: {
-      alignItems: 'center',
-      display: 'flex',
-    },
-  })),
-  ({ classes }) => (
-    <div className={classes.root}>
-      <BucketConfig.WithCurrentBucket>
-        {(bucket) => bucket
-          ? (
-            <React.Fragment>
-              <BucketDisplaySelect bucket={bucket} />
-              <Search />
-            </React.Fragment>
-          )
-          : <BucketSelect />
-        }
-      </BucketConfig.WithCurrentBucket>
-    </div>
-  ));
+export default () => {
+  const bucket = BucketConfig.useCurrentBucket();
+  return (
+    <Box display="flex" alignItems="center">
+      {bucket
+        ? (
+          <React.Fragment>
+            <BucketDisplaySelect bucket={bucket} />
+            <Search />
+          </React.Fragment>
+        )
+        : <BucketSelect />
+      }
+    </Box>
+  );
+};
