8018cb3a421fdaf62473b219af6ea2f025781080
nl_0 <nl.imbecile@gmail.com>
2019-04-11 16:18:15 +0500

    NavBar: support dividers in suggestions

--- catalog/app/utils/BucketConfig.js
+++ catalog/app/utils/BucketConfig.js
@@ -1,14 +1,17 @@
+import * as React from 'react';
+
 import AsyncResult from 'utils/AsyncResult';
 import * as Config from 'utils/Config';
 import * as NamedRoutes from 'utils/NamedRoutes';
 import { useRoute } from 'utils/router';
 
 
-export const useBucketConfigs = ({ suggestedOnly = false } = {}) => {
-  const { suggestedBuckets, federations } = Config.use();
-  return suggestedOnly
-    ? federations.filter(({ name }) => suggestedBuckets.includes(name))
-    : federations;
+export const useBucketConfigs = () => {
+  const { federations } = Config.use();
+  return React.useMemo(
+    () => federations.reduce((acc, f) => ({ ...acc, [f.name]: f }), {}),
+    [federations],
+  );
 };
 
 export const useCurrentBucket = () => {
@@ -20,15 +23,10 @@ export const useCurrentBucket = () => {
 export const useCurrentBucketConfig = () => {
   const bucket = useCurrentBucket();
   const buckets = useBucketConfigs();
-
-  return bucket
-    && (buckets.find(({ name }) => name === bucket) || { name: bucket });
+  return bucket && (buckets[bucket] || { name: bucket });
 };
 
 // compatibility
-export const WithBucketConfigs = ({ children, suggestedOnly }) =>
-  children(AsyncResult.Ok(useBucketConfigs({ suggestedOnly })));
-
 export const WithCurrentBucket = ({ children }) =>
   children(useCurrentBucket());
 
