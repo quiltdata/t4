03538a203cc1b3789efc0fc11febf41dc5100f33
Aleksey Bilogur <aleksey@residentmar.io>
2019-04-18 15:47:18 -0700

    Implement unparameterized get on a local package (#370)

--- api/python/tests/integration/test_packages.py
+++ api/python/tests/integration/test_packages.py
@@ -1018,3 +1018,52 @@ class PackageTest(QuiltTestCase):
             pkg.set('foo', './')
         with pytest.raises(QuiltException):
             pkg.set('foo', os.path.dirname(__file__))
+
+
+    def test_default_package_get_local(self):
+        foodir = pathlib.Path("foo_dir")
+        bazdir = pathlib.Path("baz_dir")
+        foodir.mkdir(parents=True, exist_ok=True)
+        bazdir.mkdir(parents=True, exist_ok=True)
+        with open('bar', 'w') as fd:
+            fd.write(fd.name)
+        with open('foo', 'w') as fd:
+            fd.write(fd.name)
+        with open(bazdir / 'baz', 'w') as fd:
+            fd.write(fd.name)
+        with open(foodir / 'bar', 'w') as fd:
+            fd.write(fd.name)
+
+        currdir = 'file://' + pathlib.Path('.').absolute().as_posix() + '/'
+
+        # consistent local case
+        pkg = t4.Package().set_dir("/", "./")
+        assert pkg.get() == currdir
+
+        # package with one inconsistent path, leading case
+        pkg = t4.Package().set_dir("/", "./")
+        pkg.set('badpath', 'bar')
+        with pytest.raises(QuiltException):
+            pkg.get()
+
+        # package with one inconsistent path, training case
+        pkg = t4.Package().set_dir("/", "./")
+        # prefix with 'z_' to ensure that this entry is last in sorted order
+        pkg.set('z_badpath', 'bar')
+        with pytest.raises(QuiltException):
+            pkg.get()
+
+        # package with inconsistent schemes
+        with patch('t4.packages.get_size_and_meta', return_value=(0, dict(), '0')):
+            pkg = t4.Package().set_dir("/", "./")
+            pkg.set("bar", "s3://test-bucket/bar")
+            with pytest.raises(QuiltException):
+                pkg.get()
+
+        # package with inconsistent root directories
+        with open('foo_dir/foo', 'w') as fd:
+            fd.write(fd.name)
+        pkg = t4.Package().set_dir("/", "./")
+        pkg.set('foo', 'foo_dir/foo')
+        with pytest.raises(QuiltException):
+            pkg.get()
