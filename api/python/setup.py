3b77652cde299eb53702e09ce978328db882619f
stevededalus <michael.sindelar@gmail.com>
2019-04-23 13:16:06 -0700

    Bump setup

--- api/python/setup.py
+++ api/python/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 def readme():
     readme_short = """
