3fb0174b319ea3cdb4315a02c020c07e8f7c0d2a
nl 0 <nl.imbecile@gmail.com>
2019-04-25 03:19:41 +0500

    Catalog: speed up package revision listing (#431)

--- catalog/app/containers/Bucket/PackageDetail.js
+++ catalog/app/containers/Bucket/PackageDetail.js
@@ -5,11 +5,12 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Typography from '@material-ui/core/Typography'
-import { withStyles } from '@material-ui/styles'
+import { makeStyles, withStyles } from '@material-ui/styles'
 
 import AsyncResult from 'utils/AsyncResult'
 import * as AWS from 'utils/AWS'
-import { withData } from 'utils/Data'
+import * as Config from 'utils/Config'
+import Data from 'utils/Data'
 import * as NamedRoutes from 'utils/NamedRoutes'
 import * as RT from 'utils/reactTools'
 
@@ -36,42 +37,37 @@ const Field = RT.composeComponent(
   ),
 )
 
-export default RT.composeComponent(
-  'Bucket.PackageDetail',
-  AWS.S3.inject(),
-  withData({
-    params: ({
-      s3,
-      match: {
-        params: { bucket, name },
-      },
-    }) => ({ s3, bucket, name }),
-    fetch: requests.getPackageRevisions,
-  }),
-  NamedRoutes.inject(),
-  withStyles(({ spacing: { unit }, palette }) => ({
-    card: {
-      marginTop: unit,
+const useStyles = makeStyles(({ spacing: { unit }, palette }) => ({
+  card: {
+    marginTop: unit,
+  },
+  link: {
+    display: 'block',
+    '&:hover': {
+      background: palette.action.hover,
     },
-    link: {
-      display: 'block',
-      '&:hover': {
-        background: palette.action.hover,
-      },
-    },
-  })),
-  ({
-    urls,
-    classes,
-    match: {
-      params: { bucket, name },
-    },
-    data: { result },
-  }) => (
-    <React.Fragment>
+  },
+}))
+
+export default ({
+  match: {
+    params: { bucket, name },
+  },
+}) => {
+  const { urls } = NamedRoutes.use()
+  const classes = useStyles()
+  const s3 = AWS.S3.use()
+  const signer = AWS.Signer.use()
+  const { apiGatewayEndpoint: endpoint } = Config.useConfig()
+
+  return (
+    <>
       <Typography variant="h4">{name}: revisions</Typography>
-      {AsyncResult.case(
-        {
+      <Data
+        fetch={requests.getPackageRevisions}
+        params={{ s3, signer, endpoint, bucket, name }}
+      >
+        {AsyncResult.case({
           _: () => <CircularProgress />,
           Err: displayError(),
           Ok: R.map(
@@ -90,9 +86,8 @@ export default RT.composeComponent(
                 </Card>
               ),
           ),
-        },
-        result,
-      )}
-    </React.Fragment>
-  ),
-)
+        })}
+      </Data>
+    </>
+  )
+}
