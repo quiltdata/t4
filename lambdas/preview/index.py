753a5bc93635e89992e553ac2fedd6b66a2eef1c
Aneesh Karve <aneesh.karve@gmail.com>
2019-04-25 18:51:25 -0700

    Revert "Drop lambdas and ci for them (#340)"
    This reverts commit eb4a1f436352bba2bf15df16c0dd5fd7684eec09.

---
+++ lambdas/preview/index.py
@@ -0,0 +1,175 @@
+"""
+Preview file types in S3 by returning preview HTML and other metadata from
+a lambda function.
+"""
+import json
+from tempfile import NamedTemporaryFile
+
+from nbconvert import HTMLExporter
+import nbformat
+import pyarrow.parquet as pq
+import requests
+
+from t4_lambda_shared.decorator import api, validate
+from t4_lambda_shared.utils import get_default_origins, make_json_response
+
+MAX_LINES = 500
+
+SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'url': {
+            'type': 'string'
+        },
+        'input': {
+            'enum': ['ipynb', 'parquet', 'vcf']
+        }
+    },
+    'required': ['url', 'input'],
+    'additionalProperties': False
+}
+
+@api(cors_origins=get_default_origins())
+@validate(SCHEMA)
+def lambda_handler(params, _):
+    """
+    dynamically handle preview requests for bytes in S3
+
+    caller must specify input_type (since there may be no file extension)
+    """
+    url = params['url']
+    input_type = params.get('input')
+
+    resp = requests.get(url)
+    if resp.ok:
+        with NamedTemporaryFile() as file_:
+            for chunk in resp.iter_content(chunk_size=1024):
+                file_.write(chunk)
+            file_.seek(0)
+
+            if input_type == 'ipynb':
+                html, info = extract_ipynb(file_)
+            elif input_type == 'parquet':
+                html, info = extract_parquet(file_)
+            elif input_type == 'vcf':
+                html, info = extract_vcf(file_)
+            else:
+                assert False
+
+        assert isinstance(html, str), 'expected html parameter as string'
+        assert isinstance(info, dict), 'expected info metadata to be a dict'
+
+        ret_val = {
+            'info': info,
+            'html': html,
+        }
+
+    else:
+        ret_val = {
+            'error': resp.reason
+        }
+
+    return make_json_response(200, ret_val)
+
+def extract_ipynb(file_):
+    """
+    parse and extract ipynb files
+
+    Args:
+        file_ - named temporary file
+
+    Returns:
+        html - html version of notebook
+        info - unmodified (is also passed in)
+    """
+    html_exporter = HTMLExporter()
+    html_exporter.template_file = 'basic'
+
+    notebook = nbformat.read(file_, 4)
+    html, _ = html_exporter.from_notebook_node(notebook)
+
+    return html, {}
+
+def extract_parquet(file_):
+    """
+    parse and extract key metadata from parquet files
+
+    Args:
+        file_ - named temporary file
+
+    Returns:
+        html - html summary of main contents (if applicable)
+        info - metdata for user consumption
+
+    """
+    # TODO: generalize to datasets, multipart files
+    # As written, only works for single files, and metadata
+    # is slanted towards the first row_group
+    meta = pq.read_metadata(file_)
+
+    info = {}
+    info['created_by'] = meta.created_by
+    info['format_version'] = meta.format_version
+    info['metadata'] = {
+        # seems silly but sets up a simple json.dumps(info) below
+        k.decode():json.loads(meta.metadata[k])
+        for k in meta.metadata
+    }
+    info['num_row_groups'] = meta.num_row_groups
+    info['schema'] = {
+        name: {
+            'logical_type': meta.schema.column(i).logical_type,
+            'max_definition_level': meta.schema.column(i).max_definition_level,
+            'max_repetition_level': meta.schema.column(i).max_repetition_level,
+            'path': meta.schema.column(i).path,
+            'physical_type': meta.schema.column(i).physical_type,
+        }
+        for i, name in enumerate(meta.schema.names)
+    }
+    info['serialized_size'] = meta.serialized_size
+    info['shape'] = [meta.num_rows, meta.num_columns]
+
+    file_.seek(0)
+    # TODO: make this faster with n_threads > 1?
+    row_group = pq.ParquetFile(file_).read_row_group(0)
+    # convert to str since FileMetaData is not JSON.dumps'able (below)
+    html = row_group.to_pandas()._repr_html_() # pylint: disable=protected-access
+
+    return html, info
+
+def extract_vcf(file_):
+    """
+    Pull summary info from VCF: meta-information, header line, and data lines
+    (in that order, up to MAX_LINES)
+    """
+
+    meta = []
+    header = []
+    data = []
+
+    with open(file_.name, 'rb') as vcf:
+        for index, line in enumerate(vcf, start=1):
+            # don't escape quotes
+            line = line.strip().decode('unicode_escape')
+            formatted = f'<p>{line}</p>'
+            if line.startswith('##'):
+                meta.append(formatted)
+            elif line.startswith('#'):
+                header.append(formatted)
+            else:
+                data.append(formatted)
+
+            if index >= MAX_LINES:
+                break
+
+    # pylint: disable=bad-continuation
+    html = ("<div>"
+                "<div data-type='meta'>{}</div>"
+                "<div data-type='header'>{}</div>"
+                "<div data-type='data'>{}</div>"
+            "</div>").format(
+                ''.join(meta) + '<br />',
+                ''.join(header) + '<br />',
+                ''.join(data))
+
+    return html, {}
---
+++ lambdas/preview/test/test_index.py
@@ -0,0 +1,87 @@
+"""
+Test functions for preview endpoint
+"""
+import json
+import pathlib
+
+import responses
+
+from ..index import lambda_handler
+
+MOCK_ORIGIN = 'http://localhost:3000'
+
+BASE_DIR = pathlib.Path(__file__).parent / 'data'
+
+# pylint: disable=no-member,invalid-sequence-index
+class TestIndex():
+    """Class to test various inputs to the main indexing function"""
+
+    FILE_URL = f'{MOCK_ORIGIN}/file.ext'
+
+    @classmethod
+    def _make_event(cls, query, headers=None):
+        return {
+            'queryStringParameters': query or None,
+            'headers': headers or None
+        }
+
+    def test_bad(self):
+        """send a known bad event (no input query parameter)"""
+        event = self._make_event({'url': self.FILE_URL}, {'origin': MOCK_ORIGIN})
+        resp = lambda_handler(event, None)
+        assert resp['statusCode'] == 400, "Expected 400 on event without 'input' query param"
+        assert resp['body'], 'Expected explanation for 400'
+        assert resp['headers']['access-control-allow-origin'] == '*'
+
+    @responses.activate
+    def test_ipynb(self):
+        """test sending ipynb bytes"""
+        notebook = BASE_DIR / 'nb_1200727.ipynb'
+        responses.add(
+            responses.GET,
+            self.FILE_URL,
+            body=notebook.read_bytes(),
+            status=200)
+        event = self._make_event({'url': self.FILE_URL, 'input': 'ipynb'})
+        resp = lambda_handler(event, None)
+        body = json.loads(resp['body'])
+        html_ = BASE_DIR / 'ipynb_html_response.txt'
+        expected = html_.read_text().strip()
+        assert body['html'].strip() == expected, \
+            f"Unexpected HTML:\n{body['html']}"
+
+    @responses.activate
+    def test_parquet(self):
+        """test sending parquet bytes"""
+        parquet = BASE_DIR / 'atlantic_storms.parquet'
+        info_response = BASE_DIR / 'parquet_info_response.json'
+        responses.add(
+            responses.GET,
+            self.FILE_URL,
+            body=parquet.read_bytes(),
+            status=200)
+        event = self._make_event({'url': self.FILE_URL, 'input': 'parquet'})
+        resp = lambda_handler(event, None)
+        assert resp['statusCode'] == 200, f"Expected 200, got {resp['statusCode']}"
+        body = json.loads(resp['body'])
+        with open(info_response, 'r') as info_json:
+            expected = json.load(info_json)
+        assert (body['info'] == expected), \
+            f"Unexpected body['info'] for {parquet}"
+
+    @responses.activate
+    def test_vcf(self):
+        """test sending vcf bytes"""
+        vcf = BASE_DIR / 'example.vcf'
+        responses.add(
+            responses.GET,
+            self.FILE_URL,
+            body=vcf.read_bytes(),
+            status=200)
+        event = self._make_event({'url': self.FILE_URL, 'input': 'vcf'})
+        resp = lambda_handler(event, None)
+        body = json.loads(resp['body'])
+        html_ = BASE_DIR / 'vcf_html_response.txt'
+        expected = html_.read_text().rstrip()
+        assert body['html'] == expected, \
+            f"Unexpected HTML:\n{body['html']}"
